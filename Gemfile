09a304fa1c82d776e8fe9cd6063ea770bf36ed99
Arthur Lee <misterarthurlee@gmail.com>
2013-04-11 21:45:47 +0900

    Add user model with Devise

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
+gem 'jquery-rails'
+gem 'devise'
+gem 'simple_form'
 
 group :production do
 	gem 'pg'
@@ -18,5 +21,5 @@ group :assets do
   gem 'bootstrap-sass', '~> 2.3.1.0'
 end
 
-gem 'jquery-rails'
+
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,9 +59,10 @@ Omrails::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
